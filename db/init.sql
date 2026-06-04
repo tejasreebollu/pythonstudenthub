eeec407ebb1a083450ac330197e8491916094e12
Aegletek <68369461+aegletek@users.noreply.github.com>
2026-06-04 20:31:32 +0530

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE IF NOT EXISTS studentdb;
+
+USE studentdb;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE
+);
